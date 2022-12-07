c05998646ede18e39c58df4b93c8e11a79d977da
ckl <1051928078@qq.com>
2022-12-07 10:44:25 +0800

    init project

---
+++ RPC-core/src/test/java/test.java
@@ -0,0 +1,10 @@
+import com.ckl.rpc.HelloWorld;
+import org.junit.Test;
+
+public class test {
+    @Test
+    public void test(){
+        HelloWorld helloWorld=new HelloWorld();
+        helloWorld.sayHello();
+    }
+}
