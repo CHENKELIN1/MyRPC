e1c3f9fbb20e6559f6bdc4ba62cdc7a539c70b42
ckl <1051928078@qq.com>
2022-12-09 16:02:53 +0800

    [v3.0]O 多线程方式注册多个服务

--- RPC-client/src/main/java/com/ckl/rpc/TestNettyClient.java
+++ RPC-client/src/main/java/com/ckl/rpc/TestNettyClient.java
@@ -2,16 +2,20 @@ package com.ckl.rpc;
 
 import com.ckl.rpc.api.HelloObject;
 import com.ckl.rpc.api.HelloService;
+import com.ckl.rpc.api.MyTest;
 import com.ckl.rpc.serializer.CommonSerializer;
 import com.ckl.rpc.transport.netty.client.NettyClient;
 
 public class TestNettyClient {
     public static void main(String[] args) {
-        RpcClient rpcClient = new NettyClient(CommonSerializer.KRYO_SERIALIZER);
+        RpcClient rpcClient = new NettyClient(CommonSerializer.JSON_SERIALIZER);
         RpcClientProxy rpcClientProxy = new RpcClientProxy(rpcClient);
         HelloService helloService = rpcClientProxy.getProxy(HelloService.class);
         HelloObject object = new HelloObject(12, "This is a message");
         String res = helloService.hello(object);
         System.out.println(res);
+
+        MyTest myTest=rpcClientProxy.getProxy(MyTest.class);
+        System.out.println(myTest.getData());
     }
 }
--- RPC-server/src/main/java/com/ckl/rpc/TestNettyServer.java
+++ RPC-server/src/main/java/com/ckl/rpc/TestNettyServer.java
@@ -1,13 +1,23 @@
 package com.ckl.rpc;
 
 import com.ckl.rpc.api.HelloService;
+import com.ckl.rpc.api.MyTest;
 import com.ckl.rpc.serializer.CommonSerializer;
 import com.ckl.rpc.transport.netty.server.NettyServer;
 
 public class TestNettyServer {
     public static void main(String[] args) {
-        HelloService helloService = new HelloServiceImpl();
-        NettyServer server = new NettyServer("127.0.0.1", 9000, CommonSerializer.KRYO_SERIALIZER);
-        server.publishService(helloService, HelloService.class);
+        Thread t1=new Thread(() ->{
+            HelloService helloService = new HelloServiceImpl();
+            NettyServer server = new NettyServer("127.0.0.1", 9000, CommonSerializer.JSON_SERIALIZER);
+            server.publishService(helloService, HelloService.class);
+        });
+        Thread t2=new Thread(() ->{
+            MyTest myTest = new MyTestImpl();
+            NettyServer server = new NettyServer("127.0.0.1", 9001, CommonSerializer.JSON_SERIALIZER);
+            server.publishService(myTest, MyTest.class);
+        });
+        t1.start();
+        t2.start();
     }
 }
