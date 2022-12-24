03dd807c13008ed743bddb450bfd7401f65a78ca
ckl <1051928078@qq.com>
2022-12-25 00:52:13 +0800

    [v5.0]U 修改初始化logo；

--- RPC-common/src/main/java/com/ckl/rpc/config/Init.java
+++ RPC-common/src/main/java/com/ckl/rpc/config/Init.java
@@ -13,27 +13,4 @@ public class Init {
         System.out.println("|_____||_____|[\\_:  /|____| |___||_____|    `.____ .' ");
         System.out.println("               \\__.'                                  ");
     }
-    private static void initLogo2(){
-        System.out.println(" ......................我佛慈悲......................");
-        System.out.println("                       _oo0oo_                      ");
-        System.out.println("                      o8888888o                     ");
-        System.out.println("                      88\" . \"88                     ");
-        System.out.println("                      (| -_- |)                     ");
-        System.out.println("                      0\\  =  /0                     ");
-        System.out.println("                    ___/‘---’\\___                   ");
-        System.out.println("                  .' \\|       |/ '.                 ");
-        System.out.println("                 / \\\\|||  :  |||// \\                ");
-        System.out.println("                / _||||| -卍-|||||_ \\               ");
-        System.out.println("               |   | \\\\\\  -  /// |   |              ");
-        System.out.println("               | \\_|  ''\\---/''  |_/ |              ");
-        System.out.println("               \\  .-\\__  '-'  ___/-. /              ");
-        System.out.println("             ___'. .'  /--.--\\  '. .'___            ");
-        System.out.println("          .\"\" ‘<  ‘.___\\_<|>_/___.’ >’ \"\".          ");
-        System.out.println("         | | :  ‘- \\‘.;‘\\ _ /’;.’/ - ’ : | |        ");
-        System.out.println("         \\  \\ ‘_.   \\_ __\\ /__ _/   .-’ /  /        ");
-        System.out.println("     =====‘-.____‘.___ \\_____/___.-’___.-’=====     ");
-        System.out.println("                       ‘=---=’                      ");
-        System.out.println("                                                    ");
-        System.out.println("....................佛祖开光 ,永无BUG...................");
-    }
 }
